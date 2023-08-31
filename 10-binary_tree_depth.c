73dd51178215f52eabb1faa5f42a99836351201f
catherinekimani <catherinekimani882@gmail.com>
2023-08-31 13:35:25 +0300

    Check betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,6 +3,8 @@
 /**
  * binary_tree_depth - func that measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
+ *
+ * Return: 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h, right_h;
+	size_t left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
-
-	return (1 + (left_h > right_h ? left_h : right_h));
+	if (tree->left)
+		left_h += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_h += 1 + binary_tree_height(tree->right);
+	if (left_h > right_h)
+		return (left_h);
+	else
+		return (right_h);
 }
